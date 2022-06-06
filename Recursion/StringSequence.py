26ff0ea15a7ad39d8982db4d5c4330d451de63a6
= <=>
2022-06-06 08:40:02 +0530

    Added String sequence program

---
+++ Recursion/StringSequence.py
@@ -0,0 +1,20 @@
+from ctypes import pointer
+
+
+def stringSequence(str, powerset, output, i):
+    if i == len(str):
+        output_str = ''.join(output)
+        powerset.append(output_str)
+        return
+    else:
+        output_cpy = list(output)
+        stringSequence(str, powerset, output_cpy, i+1)
+        output.append((str[i]))
+        output_cpy = list(output)
+        stringSequence(str, powerset, output_cpy, i+1)
+
+str = 'bbb'
+powerset = [[]]
+stringSequence(str, powerset, [], 0)
+powerset = powerset[2:]
+print(powerset)
