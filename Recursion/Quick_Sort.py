9d7da587775d19077bb027f6597360262cfb507f
= <=>
2022-09-05 09:30:08 +0530

    Optimised Quick Sort

--- Recursion/Quick_Sort.py
+++ Recursion/Quick_Sort.py
@@ -1,33 +1,31 @@
-def quick_sort(arr, i, j):
-    i_cpy = i
-    j_cpy = j
-    if i >= j:
-        return
-    pivot = i
-    lesser_elements = 0
-    for k in range(i, j+1):
-        if arr[k] < arr[pivot]:
-            lesser_elements += 1
-    # making pivot to its appropriate position
-    new_pivot  = pivot + lesser_elements
-    temp = arr[pivot]
-    arr[pivot] = arr[pivot + lesser_elements]
-    arr[pivot + lesser_elements] = temp
-    # checking right elements are lesser than pivot and greater elements are left side of pivot otherwise swapping
-    while i <= j and i < new_pivot and j > new_pivot:
-        if arr[i] < arr[new_pivot]:
+def quick_sort(arr, start, end):
+    if start >= end :
+        return 
+    pivot = partition(arr, start, end)
+    quick_sort(arr, start, pivot-1)
+    quick_sort(arr, pivot+1, end)
+
+def partition(arr, start, end):
+    pivot = start
+    counter =  0
+    for i in arr[start:end+1]:
+        if i < arr[pivot]:
+            counter += 1
+    arr[pivot], arr[start + counter] = arr[start + counter], arr[pivot]
+    pivot = start + counter
+    i = start
+    j = end
+    while i < pivot and j > pivot:
+        if arr[i] > arr[pivot] and arr[j] < arr[pivot]:
+            arr[i], arr[j] = arr[j], arr[i]
             i += 1
-        if arr[j] >= arr[new_pivot]:
             j -= 1
-        if arr[i] >= arr[new_pivot] and arr[j] < arr[new_pivot]:
-            temp = arr[i]
-            arr[i] = arr[j]
-            arr[j] = temp
-            j -= 1
+            continue
+        if arr[i] <= arr[pivot]:
             i += 1
-    quick_sort(arr, i_cpy, new_pivot-1)
-    quick_sort(arr, new_pivot+1, j_cpy)
-lst = [6,6,5]
-quick_sort(lst, 0, len(lst)-1)
-print(lst)
-#Done
+        if arr[j] > arr[pivot]:
+            j -= 1
+    return pivot
+arr = [2,1,23]
+quick_sort(arr, 0,2)
+print(arr)
